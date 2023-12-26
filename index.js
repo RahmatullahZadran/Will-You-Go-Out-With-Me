17a68970430900b1f4145d72ae44289ab1c3b99c
Rahmatullah Zadran <rahmatullahzadran@outlook.com>
2023-12-26 23:48:44 +0000

    html complete, bug fixing on JC

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ function changePicture() {
     var image = document.getElementById("image");
     image.src = "images/Pic2.png";
 }
+
+function moveButton() {
+    var button = document.getElementById("no");
+    button.style.transform = "translate(-20px, -20px)";
+}
+
+function startButton() {
+    intervalId = setInterval(moveButton, 50);
+
+}
+ 
+function stopButton() {
+    clearInterval(intervalId);
+    var button = document.getElementById("no");
+    button.style.transform = "translate(0px, 0px)";
+}
